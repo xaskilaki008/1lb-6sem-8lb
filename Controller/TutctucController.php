83f545aab0816edf15fefa76c7b1b7f7ab51b318
Petya <206858385+xaskilaki008@users.noreply.github.com>
2025-07-27 18:09:20 +0300

    Revert "+"
    This reverts commit 2fc0f9444f652c4687eb9a39593d6a8d638df00d.

--- Controller/TutctucController.php
+++ Controller/TutctucController.php
@@ -3,11 +3,10 @@ require_once __DIR__ . '/../Model/Tutctuc.php';
 
 class TutctucController {
     public function index() {
-        //Через создание экземпляра модели
-        $model = new Tutctuc(); // Используется значение из константы ->
-    // $helloText = $model->getMessage();
-    // Прямой доступ к константе без создания экземпляра->
-        $helloText = Tutctuc::MessagerHello;
+        $model = new Tutctuc("Hello world from Model!");
+        
+        // Передаем сообщение из модели в view
+        $helloText = $model->getMessage();
         
         require __DIR__ . '/../View/tutctuc_view.php';
     }
--- Model/Tutctuc.php
+++ Model/Tutctuc.php
@@ -1,20 +1,12 @@
 <?php
 class Tutctuc {
-    const MessagerHello = "Hello world from Model Constant!";
-    
     private $message;
 
-    public function __construct($message = null) {
-        // сообщение из параметра или из константы, если параметр не указан
-        $this->message = $message ?? self::MessagerHello;
+    public function __construct($message) {
+        $this->message = $message;
     }
 
     public function getMessage() {
         return $this->message;
-    }
-    
-    // метод для прямого доступа к константе
-    public static function getDefaultMessage() {
-        return self::MessagerHello;
     }
 }
